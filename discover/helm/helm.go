4ec90a54ffa4b82dd24acba30b53266e0d10bc1a
Dee Kryvenko <llibicpep@gmail.com>
2020-08-18 14:28:07 -0700

    add helm

---
+++ discover/helm/helm.go
@@ -0,0 +1,28 @@
+package helm
+
+import (
+	"runtime"
+
+	"github.com/plumber-cd/runtainer/discover"
+	"github.com/spf13/viper"
+)
+
+// Discover specific to Go
+func Discover() {
+	// get what's already calculated by now
+	h, i, v := discover.GetFromViper()
+
+	v.AddMirrorHostMount(h, i, "~/.helm")
+
+	switch runtime.GOOS {
+	case "darwin":
+		v.AddHostMount(h, i, "~/Library/Preferences/helm", "~/.config/helm")
+		v.AddHostMount(h, i, "~/Library/Caches/helm", "~/.cache/helm")
+	default:
+		v.AddMirrorHostMount(h, i, "~/.config/helm")
+		v.AddMirrorHostMount(h, i, "~/.cache/helm")
+	}
+
+	// publish what we've calculated to viper
+	viper.Set("volumes", v)
+}
